d51c5bcef684e6c3c6c7b63cd582413c99e81fd1
Lali Bermudez <laliberbello@gmail.com>
2018-05-03 16:31:29 -0500

    Add first 5 sites to the array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 var sites = [
 	{
-		title: "",
-		location: {lat: , lng: }
+		title: "Panama Viejo - Old Panama",
+		location: {lat: 9.00677, lng: -79.487071},
+		map: map,
+		animation:
+	},
+	{
+		title: "Casco Antiguo",
+		location: {lat: 8.953272, lng: -79.533692},
+		map: map,
+		animation:
+	},
+	{
+		title: "Panama Canal",
+		location: {lat: 9.022348, lng: -79.616695},
+		map: map,
+		animation:
+	},
+	{
+		title: "Causeway",
+		location: {lat: 8.917218, lng: -79.533039},
+		map: map,
+		animation:
+	},
+	{
+		title: "Tocumen International Airport",
+		location: {lat: 9.067526, lng: -79.387147},
 		map: map,
 		animation:
 	}
