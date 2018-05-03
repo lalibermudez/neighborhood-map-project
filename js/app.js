150fdc5075022cd3f17aaa680bed8af65a10ea6a
Lali Bermudez <laliberbello@gmail.com>
2018-05-03 17:12:32 -0500

    Display sites names on list

--- js/app.js
+++ js/app.js
@@ -3,35 +3,38 @@ var sites = [
 		title: "Panama Viejo - Old Panama",
 		location: {lat: 9.00677, lng: -79.487071},
 		map: map,
-		animation:
+		animation: ""
 	},
 	{
 		title: "Casco Antiguo",
 		location: {lat: 8.953272, lng: -79.533692},
 		map: map,
-		animation:
+		animation: ""
 	},
 	{
 		title: "Panama Canal",
 		location: {lat: 9.022348, lng: -79.616695},
 		map: map,
-		animation:
+		animation: ""
 	},
 	{
 		title: "Causeway",
 		location: {lat: 8.917218, lng: -79.533039},
 		map: map,
-		animation:
+		animation: ""
 	},
 	{
 		title: "Tocumen International Airport",
 		location: {lat: 9.067526, lng: -79.387147},
 		map: map,
-		animation:
+		animation: ""
 	}
-] 
+];
 
 var AppViewModel = function() {
+	var self = this;
+
+	this.sitesList = ko.observableArray();
 
 };
 
