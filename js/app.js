54a17755d2f907bef4b70bfd49b6e8d97926e477
Lali Bermudez <laliberbello@gmail.com>
2018-05-03 15:21:16 -0500

    Creating script tag for map in index.html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-var map;
+// var map;
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.7413549, lng: -73.9980244},
-		zoom: 13
-	});
-}
+// function initMap() {
+//     // Constructor creates a new map - only center and zoom are required.
+//     map = new google.maps.Map(document.getElementById('map'), {
+// 		center: {lat: 40.7413549, lng: -73.9980244},
+// 		zoom: 13
+// 	});
+// };
 
-initMap();
+// initMap();
        
