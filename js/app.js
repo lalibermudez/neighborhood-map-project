f7971e540a030264829441f2e43bb5e3a44e61fa
Lali Bermudez <laliberbello@gmail.com>
2018-05-04 15:23:14 -0500

    Display markers on app

--- js/app.js
+++ js/app.js
@@ -32,46 +32,37 @@ var sites = [
 ];
 
 var map;
+var markers = [];
 
 function initMap() {
 	// Constructor creates a new map
 	map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: 9.00876, lng: -79.520244},
-	  zoom: 11
+		center: {lat: 9.00876, lng: -79.520244},
+		zoom: 11
 	});
+
+	// The following group uses the sites array to create an array of markers on initialize.
+	for (var i = 0; i < sites.length; i++) {
+		// Get the position from the sites array.
+		var position = sites[i].location;
+		var title = sites[i].title;
+		// Create a marker per site, and put into markers array.
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			id: i
+		});
+		// Push the marker to our array of markers.
+		markers.push(marker);
+	};
 };
+
 
 var AppViewModel = function() {
 	var self = this;
 
-	self.markers = [];
-
-	// self.placeMarkers = function() {
-	// 	// The following group uses the sites array to create an array of markers on initialize.
-		// for (var i = 0; i < sites.length; i++) {
-	// 		// Get the position from the sites array.
-	// 		var position = sites[i].location;
-	// 		var title = sites[i].title;
-	// 		// Create a marker per site, and put into markers array.
-	// 		var marker = new google.maps.Marker({
-	// 			map: map,
-	// 			position: position,
-	// 			title: title,
-	// 			animation: google.maps.Animation.DROP,
-	// 			id: i
-	// 		});
-	// 		// Push the marker to our array of markers.
-	// 		markers.push(marker);
-	// 	};
-	// };
-
-	// createMarkers();
-
-        // var marker = new google.maps.Marker({
-        //   position: {lat: 9.067526, lng: -79.387147},
-        //   map: map,
-        //   title: 'First Marker!'
-        // });
 };
 
 
