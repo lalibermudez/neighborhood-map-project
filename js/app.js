153fac28f2117bed135cbd3b2d9d67e276d4c14c
Lali Bermudez <laliberbello@gmail.com>
2018-05-04 15:34:08 -0500

    Display letters on markers and list

--- js/app.js
+++ js/app.js
@@ -3,31 +3,31 @@ var sites = [
 		title: "Panama Viejo - Old Panama",
 		location: {lat: 9.00677, lng: -79.487071},
 		map: map,
-		animation: ""
+		label : "A"
 	},
 	{
 		title: "Casco Antiguo",
 		location: {lat: 8.953272, lng: -79.533692},
 		map: map,
-		// animation: ""
+		label : "B"
 	},
 	{
 		title: "Panama Canal",
 		location: {lat: 9.022348, lng: -79.616695},
 		map: map,
-		// animation: ""
+		label : "C"
 	},
 	{
 		title: "Causeway",
 		location: {lat: 8.917218, lng: -79.533039},
 		map: map,
-		// animation: ""
+		label : "D"
 	},
 	{
 		title: "Tocumen International Airport",
 		location: {lat: 9.067526, lng: -79.387147},
 		map: map,
-		// animation: ""
+		label : "E"
 	}
 ];
 
@@ -46,11 +46,13 @@ function initMap() {
 		// Get the position from the sites array.
 		var position = sites[i].location;
 		var title = sites[i].title;
+		var label = sites[i].label;
 		// Create a marker per site, and put into markers array.
 		var marker = new google.maps.Marker({
 			map: map,
 			position: position,
 			title: title,
+			label: label,
 			animation: google.maps.Animation.DROP,
 			id: i
 		});
@@ -64,6 +66,5 @@ var AppViewModel = function() {
 	var self = this;
 
 };
-
 
 ko.applyBindings(new AppViewModel()); 
