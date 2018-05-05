c389a83f572f269910238bbe1d941a5f60e582cd
Lali Bermudez <laliberbello@gmail.com>
2018-05-04 20:33:07 -0500

    Display infoWindows on marker's click

--- js/app.js
+++ js/app.js
@@ -34,12 +34,14 @@ var sites = [
 var map;
 var markers = [];
 
-function initMap() {
+var initMap = function() {
 	// Constructor creates a new map
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 9.00876, lng: -79.520244},
 		zoom: 11
 	});
+
+	var largeInfowindow = new google.maps.InfoWindow();
 
 	// The following group uses the sites array to create an array of markers on initialize.
 	for (var i = 0; i < sites.length; i++) {
@@ -58,12 +60,34 @@ function initMap() {
 		});
 		// Push the marker to our array of markers.
 		markers.push(marker);
+		// Create an onclick event to open an infowindow at each marker.
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfowindow);
+		});
 	};
 };
 
+var populateInfoWindow = function(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+	};
+};
 
 var AppViewModel = function() {
 	var self = this;
+
+	// self.populateInfoWindow = function() {
+	// 	var infoWindow = new google.maps.InfoWindow({
+	// 		content: sites.title
+	// 	});
+	// }
 
 };
 
