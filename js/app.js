d3e597a0ed201f5d31c4a617459fc50a4b6f62c7
Lali Bermudez <laliberbello@gmail.com>
2018-05-09 08:01:59 -0500

    Set media queries for larger devices

--- js/app.js
+++ js/app.js
@@ -82,30 +82,10 @@ var mapError = function() {
 	$('#map').html('<div class=map-error>Oops! Could not load Google Map. Try again later.</div>')
 };
 
-// Function to display list menu when the menu icon is clicked
-// $menuIcon.click(function() {
-// 	if ($hiddenMenu.css('width') === 0) {
-// 		$hiddenMenu.css({width: '300px'});
-// 	} else {
-// 		$hiddenMenu.css({width: '0'});
-// 	}
-// });
-
 $menuIcon.click(function(e) {
 	$hiddenMenu.toggleClass('open');
 	e.stopPropagation();
 });
-
-// $menuIcon.click(function() {
-// 	$hiddenMenu.css({width: '300px'});
-// 	$menuIcon.click(function() {
-// 		$hiddenMenu.css({width: '0px'});
-// 	});
-// });
-
-
-
-
 
 // Function to display infoWindow when the marker gets clicked
 var clickMarker = function(marker) {
